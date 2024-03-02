f6401eac3dfec7d1e100d85bfe818e331cb7227f
devzeld <zeld.exe.dev@gmail.com>
2024-03-02 18:15:31 +0100

    setup

--- src/main/java/me/zeld/luny/Luny.java
+++ src/main/java/me/zeld/luny/Luny.java
@@ -1,8 +1,10 @@
 package me.zeld.luny;
 
+import me.zeld.luny.UI.GuiItem;
 import me.zeld.luny.item.ModItem;
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.item.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +12,8 @@ public class Luny implements ModInitializer {
 
 	public static final String MOD_ID = "luny";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static Item GUI_ITEM = new GuiItem(new Item.Settings());
 
 	@Override
 	public void onInitialize() {
---
+++ src/main/java/me/zeld/luny/UI/GuiItem.java
@@ -0,0 +1,10 @@
+package me.zeld.luny.UI;
+
+import net.minecraft.item.Item;
+
+public class GuiItem extends Item {
+    public GuiItem(Settings settings) {
+        super(settings);
+    }
+
+}
